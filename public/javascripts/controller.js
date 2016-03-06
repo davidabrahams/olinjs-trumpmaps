0099c9d9d3317834ecddc0b277a731811e1ba25a
David Abrahams <david.abrahams@students.olin.edu>
2016-03-06 00:45:20 -0500

    trump links work

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -3,9 +3,8 @@ var app = angular.module('app', []);
 app.controller('home', function ($scope, $filter, $http) {
 
   var myLatlng = {lat: 42.360, lng: -71.058};
+  var markers = [];
   $scope.formData = {};
-  // $scope.formData.latitude = 39.5;
-  // $scope.formData.longitude = -98.350;
 
   $scope.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
@@ -16,17 +15,29 @@ app.controller('home', function ($scope, $filter, $http) {
     $http.post("api/trump", {
       name: $scope.formData.name,
       img: $scope.formData.img,
-
+      latLng: $scope.formData.latLng
     }).then(onSuccess);
   };
 
   google.maps.event.addListener($scope.map, 'dblclick', function(event) {
     $scope.$apply(function () {
       $scope.formData.latLng = event.latLng;
+      var marker = new google.maps.Marker({
+        position: event.latLng,
+        label: 'TRUMP',
+        map: $scope.map
+      });
     });
   });
 
-  function addMarker(location, map) {
+  function addMarker(location, map, img) {
+
+    var contentString = '<img src="' + img + '">'
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
     // Add the marker at the clicked location, and add the next-available label
     // from the array of alphabetical characters.
     var marker = new google.maps.Marker({
@@ -34,8 +45,26 @@ app.controller('home', function ($scope, $filter, $http) {
       label: 'TRUMP',
       map: map
     });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+    return marker;
+  }
+
+  function clear_markers() {
+    markers.forEach( function(marker) {
+      marker.setMap(null);
+    });
   }
 
   var onSuccess = function(data) {
+    var trumps = data.data;
+    clear_markers();
+    trumps.forEach( function (trump) {
+      var m = addMarker(trump.latLng, $scope.map, trump.img);
+      markers.push(m);
+    });
   };
+
+  $http.get('api/trump').then(onSuccess);
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,22 +10,20 @@ router.get('/', function(req, res, next) {
 
 /* GET all trumps in the world. */
 router.get('/api/trump', function (req, res, next) {
-  Trump.find(function (err, topics) {res.json(topics)});
+  Trump.find(function (err, trumps) {
+    res.json(trumps)});
 });
 
 /* POST a new trump to the DB */
 router.post('/api/trump', function(req, res, next) {
   var name = req.body.name;
   var img = req.body.img;
-  var lat = req.body.lat;
-  var lon = req.body.lon;
-
-  Trump.create({ name: name, img:img, lat:lat, lon:lon}, function (err, trump) {
+  var latLng = req.body.latLng;
+  Trump.create({ name: name, img:img, latLng:latLng}, function (err, trump) {
     if (err) return res.status(500).
       send('An error occurred when creating the new trump.');
     Trump.find(function (err, trumps) {res.json(trumps)});
   });
-  
 });
 
 module.exports = router;
