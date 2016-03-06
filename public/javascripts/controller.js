71ffd7d14d5bac126cf38667baa082b4b9eb23bc
David Abrahams <david.abrahams@students.olin.edu>
2016-03-05 22:59:14 -0500

    google maps works with angular

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,3 +1,27 @@
 var app = angular.module('app', []);
 
+app.controller('home', function ($scope, $filter, $http) {
 
+  var myLatlng = {lat: 42.360, lng: -71.058};
+
+  $scope.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatlng
+  });
+
+  google.maps.event.addListener($scope.map, 'dblclick', function(event) {
+    $('#latitude').val(event.latLng.lat());
+    $('#longitude').val(event.latLng.lng());
+    addMarker(event.latLng, $scope.map);
+  });
+
+  function addMarker(location, map) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    var marker = new google.maps.Marker({
+      position: location,
+      label: 'TRUMP',
+      map: map
+    });
+  }
+});
